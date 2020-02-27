30287a3c628d631a2d7af40c742d3dfc6d2e30ba
Felipy Camargo de Souza <felipy.souza@itajai.quay.com.br>
2020-02-27 17:53:08 -0300

    exercicios

--- regex/index.js
+++ regex/index.js
@@ -94,6 +94,58 @@ contato@origamid.com`.replace(/^\w+/gm, 'X');
 contato@origamid.com`.replace(/\n/g, 'X');
 'andre@origamid.com ©'.replace(/\u0040|\u00A9/g, '---');
 
+'JavaScript'.replace(/a/, 'B')
+// BavaScript
+'JavaScript'.replace(/Java/, 'Type')
+// TypeScript
+'JavaScript'.replace(/a/g, 'i')
+// JiviScript
+'Perdeu perdido'.replace(/Pe/gi, 'Ba')
+// Bardeu Bardido
+'JavaScript'.replace(/a/g, 'i')
+// JuvuScrupt
+
+'JavaScript'.replace(/[ai]/gi, 'u');
+'111.222-333-44'.replace(/[-.]/g, '');
+'Brasil é com z: Brazil'.replace(/Bra[sz]il/g, 'Prazer');
+'JavaScript é a linguagem.'.replace(/[a-z]/g, '0');
+'JavaScript é a linguagem.'.replace(/[a-zA-Z]/g, '1');
+'123.333.333-33'.replace(/[0-9]/g, 'X');
+'Brasil é com z: Brazil'.replace(/[^a-z]/g, ' ');
+'JavaScript é a linguagem.'.replace(/./g, '0');
+'999.222.222.11'.replace(/\./g, '-');
+'Guarda-chuva R$ 23,00.'.replace(/\w/g, '-');
+'Guarda-chuva R$ 23,00.'.replace(/\W/g, '-');
+'+55 (21) 2222-2222'.replace(/\d/g, 'X');
+'+55 (21) 2222-2222'.replace(/\D/g, '');
+'+55 (21) 2222-  2222  '.replace(/\s/g, '');
+'+55 (21) 2222-  2222  '.replace(/\S/g, '');
+'Vaaaai ali por favor.'.replace(/aaaa/g, 'a');
+'Vaaaai ali por favor.'.replace(/a{4}/g, 'a');
+'222.333.222.42'.replace(/\d{2,3}/g, 'X');
+'A melhor linguagem é JavaScript'.replace(/\w{1,}/g, 'X');
+'222.333.222.42'.replace(/\d+/g, 'X');
+'Dígitos, dados, desenhos, Dito, d'.replace(/d\w+/g, 'X');
+'Dígitos, dados, desenhos, Dito, d'.replace(/d\w*/g, 'X');
+'Qual é o certo, regexp ou regex?'.replace(/regexp?/g, 'Regular Expression');
+'PHP e Java são linguagens diferentes'.replace(/java|php/gi, 'X');
+'Java não é JavaScript.'.replace(/java/gi, 'X');
+'Java não é JavaScript.'.replace(/\b\d+\b/gi, 'X');
+'O Restaurante25 na Rua 3, custa R$ 32,00'.replace(/\b\d+\b/gi, 'X');
+'11_22 33-44 55é66 77e88'.replace(/\b\d+\b/gi, 'X');
+`andre@origamid.com
+contato@origamid.com`.replace(/^\w+/g, 'X');
+`andre@origamid.com
+contato@origamid.com`.replace(/\w+$/g, 'X');
+`andre@origamid.com
+contato@origamid.com`.replace(/\w+$/gm, 'X');
+`andre@origamid.com
+contato@origamid.com`.replace(/^\w+/gm, 'X');
+`andre@origamid.com\ncontato@origamid.com`.replace(/\n/g, '---');
+`andre@origamid.com
+contato@origamid.com`.replace(/\n/g, 'X');
+'andre@origamid.com ©'.replace(/\u0040|\u00A9/g, '---');
+
 
 
 
