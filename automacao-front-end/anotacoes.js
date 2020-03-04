d72b2665cb857e3d0bfeecbbe05d0eaf423654c1
Felipy Camargo de Souza <felipy.souza@itajai.quay.com.br>
2020-03-04 15:49:53 -0300

    automação front-end v0

---
+++ automacao-front-end/anotacoes.js
@@ -0,0 +1,21 @@
+
+/*
+    Npm
+    Node Package Manager
+    gerenciador de pacotes
+    feito para Node.js
+
+    cli
+    npm install lodash
+    node
+    .exitclear
+    npm -v
+    npm -h
+    npm init
+    npm install
+    npm i eslint -g
+    npm unistall lodash
+    npm update lodash
+    npm i -D webpack webpack-cli
+    npm install --save-dev
+    */
